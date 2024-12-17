28c605d3f51311a4365f8afd4c19edc1b29254f6
Saideekshitha <104785671+Saideekshithach@users.noreply.github.com>
2024-12-17 20:13:19 +0530

    Add files via upload

---
+++ dec-11.py
@@ -0,0 +1,42 @@
+#file handling
+'''a=open("deekshi.txt","w")
+b=a.write("codegnan it solutions")
+a.close()'''
+
+'''a=open("deekshi.txt","w")
+b=a.write("python")
+a.close()'''
+
+'''a=open("deekshi.txt","a")
+b=a.write("hello")
+a.close()'''
+
+'''d=input("enter data")
+a=open("deek.txt","w")
+a.write(d)
+a.close()'''
+
+
+'''#read()
+a=open("deek.txt")
+#print(a.read())#it will display entire content
+#print(a.readline())#it will display single line
+#print(a.read(8))
+print(a.readlines())'''
+
+'''#writelines
+a=open("py.txt","w")
+b=["python","java","html","css"]
+#a.writelines(b)
+a.writelines("\n".join(b))
+a.close()'''
+
+'''
+a=open("C:\\Users\\asus\\OneDrive\\Desktop\\python 104\\add.py")
+print(a.read())'''
+
+'''a=open("ATM.py")
+print(a.read())'''
+
+a=open("C:\\Users\\asus\\Downloads\\examples sa.py")
+print(a.read())
